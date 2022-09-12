c5f9b9806cd1bca08c9289443062526d28c9b70e
wastegate711 <evgeni.shvec@gmail.com>
2022-09-12 14:18:53 +0300

    Добавил функции CreateDirectory

--- Kernel32/WinApi.Kernel32/Kernel32_lib.cs
+++ Kernel32/WinApi.Kernel32/Kernel32_lib.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using System.Security.Permissions;
 using System.Text;
 
 namespace WinApi.Kernel32
@@ -34,8 +35,8 @@ namespace WinApi.Kernel32
         /// </summary>
         /// <param name="handle">Дескриптор.</param>
         /// <returns>Вернет не нулевой результат при успешном срабатывании.</returns>
-        [DllImport(LibraryName,SetLastError = true)]
-        [return:MarshalAs(UnmanagedType.Bool)]
+        [DllImport(LibraryName, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool CloseHandle(IntPtr handle);
 
         /// <summary>
@@ -44,14 +45,97 @@ namespace WinApi.Kernel32
         /// <param name="lpExistingFileName">Имя существующего файла.</param>
         /// <param name="lpNewFileName">Имя нового файла.</param>
         /// <param name="bFailIfExists">Файл существует?</param>
-        /// <returns></returns>
-        [DllImport(LibraryName,SetLastError = true)]
-        [return:MarshalAs(UnmanagedType.Bool)]
+        /// <returns>Вернет не нулевой результат при успешном срабатывании.</returns>
+        [DllImport(LibraryName, SetLastError = true, CharSet = CharSet.Ansi)]
+        [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool CopyFile(
             [param: MarshalAs(UnmanagedType.LPStr)]
             string lpExistingFileName,
             [param: MarshalAs(UnmanagedType.LPStr)]
             string lpNewFileName,
             [param: MarshalAs(UnmanagedType.Bool)] bool bFailIfExists);
+
+        /// <summary>
+        /// Копирует существующий файл в новый файл в кодировке UNICODE.
+        /// </summary>
+        /// <param name="existingFileName">Имя существующего файла.</param>
+        /// <param name="newFileName">Имя нового файла</param>
+        /// <param name="failIfExists">Файл существует?</param>
+        /// <returns>Вернет не нулевой результат при успешном срабатывании.</returns>
+        [DllImport(LibraryName, SetLastError = true, CharSet = CharSet.Unicode)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool CopyFileW(
+            [param: MarshalAs(UnmanagedType.LPWStr)]
+            string existingFileName,
+            [param: MarshalAs(UnmanagedType.LPWStr)]
+            string newFileName,
+            [param: MarshalAs(UnmanagedType.Bool)] bool failIfExists);
+
+        /// <summary>
+        /// Создает директорию в кодировке ANSI
+        /// </summary>
+        /// <param name="pathName">Путь.</param>
+        /// <param name="securityDescriptor">Дескриптор безопасности.</param>
+        /// <returns>Вернет не нулевой результат при успешном срабатывании.</returns>
+        [DllImport(LibraryName, SetLastError = true, CharSet = CharSet.Ansi)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool CreateDirectoryA(
+            [param: MarshalAs(UnmanagedType.LPStr)]
+            string pathName,
+            SecurityAttribute securityDescriptor);
+
+        /// <summary>
+        /// Создает директорию.
+        /// </summary>
+        /// <param name="templateDirectory">Путь к директории.</param>
+        /// <param name="newDirectory">Новая директория.</param>
+        /// <param name="securityAttributes">Дескриптор безопасности.</param>
+        /// <returns>Вернет не нулевой результат при успешном срабатывании.</returns>
+        [DllImport(LibraryName, SetLastError = true, CharSet = CharSet.Unicode)]
+        [return:MarshalAs(UnmanagedType.Bool)]
+        public static extern bool CreateDirectoryExW(
+            [param:MarshalAs(UnmanagedType.LPWStr)] string templateDirectory,
+            [param:MarshalAs(UnmanagedType.LPWStr)] string newDirectory,
+            SecurityAttribute securityAttributes);
+
+        /// <summary>
+        /// Создает директорию.
+        /// </summary>
+        /// <param name="templateDirectory">Путь к директории.</param>
+        /// <param name="newDirectory">Новая директория.</param>
+        /// <param name="securityAttributes">Дескриптор безопасности.</param>
+        /// <returns>Вернет не нулевой результат при успешном срабатывании.</returns>
+        [DllImport(LibraryName,SetLastError = true,CharSet = CharSet.Ansi)]
+        [return:MarshalAs(UnmanagedType.Bool)]
+        public static extern bool CreateDirectoryExA(
+            [param:MarshalAs(UnmanagedType.LPStr)] string templateDirectory,
+            [param:MarshalAs(UnmanagedType.LPStr)] string newDirectory,
+            SecurityAttribute securityAttributes);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
