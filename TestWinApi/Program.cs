5932f5841b4a0dccce194b0c83b3701238a7751d
wastegate711 <evgeni.shvec@gmail.com>
2024-07-27 14:34:32 +0300

    Проверил работу функции GetCursorPos все работает

--- TestWinApi/Program.cs
+++ TestWinApi/Program.cs
@@ -1,7 +1,9 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Drawing;
+using System.Runtime.InteropServices;
 using System.Text;
 using WinApi.Kernel32;
 using WinApi.Kernel32.Enums;
+using WinApi.User32;
 
 namespace TestWinApi
 {
@@ -12,8 +14,8 @@ namespace TestWinApi
             //bool res = Kernel32_lib.CreateDirectoryA("d:\\3", IntPtr.Zero);
             //Console.WriteLine(res);
             //bool result = Kernel32_lib.RemoveDirectoryW("d:\\3");
-            
-            
+
+
             //if(result)
             //    Console.WriteLine("Операция успешна.");
             //else
@@ -24,7 +26,20 @@ namespace TestWinApi
             //}
 
             Console.WriteLine(sizeof(bool));
+            var t = new Thread(test);
+            t.Start();
+        }
 
+        static void test()
+        {
+            Point point = new Point();
+
+            while (true)
+            {
+                User32.GetCursorPos(out point);
+                Console.WriteLine("X={0} Y={1}" ,point.X, point.Y);
+                Thread.Sleep(10);
+            }
         }
     }
 }
